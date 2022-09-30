349ff6e8a51c49a979b38981f108aed09a392773
Indrico <indrico.jowensen@gmail.com>
2022-09-30 15:36:19 +0700

    Initial Commit

--- index.js
+++ index.js
@@ -4,27 +4,39 @@
 // init project
 var express = require('express');
 var app = express();
+require("dotenv").config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/:date', function (req, res) {
+  const { date } = req.params;
+  let date1 = new Date(date);
+  if ((Object.prototype.toString.call(date1) === '[object Date]' && date1 != "Invalid Date")) {
+    res.json({
+      unix: Math.floor(date1.getTime()),
+      utc: date1.toGMTString(),
+    })
+  }
+  else {
+    res.json({
+      unix: parseInt(date),
+      utc: new Date(parseInt(date)).toGMTString()
+    })
+  }
+  res.json({ unix, utc });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
